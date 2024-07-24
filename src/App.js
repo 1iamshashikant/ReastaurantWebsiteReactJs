2895796b7059eba8882b7bf9ef90dc64cf37fe38
Shashi Kant <130560334+1iamshashikant@users.noreply.github.com>
2024-07-24 13:02:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import Header from './components/Layout/Header';
+import './App.css';  
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+     
+     
+    
+  );
+}
+
+export default App;
